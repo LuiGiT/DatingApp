381089d4c112bbc11086eda0d71a5155a6c30a92
luigiantoine <lantoine@consultec-ti.com>
2025-04-07 23:26:23 -0400

    End of section 8

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -10,14 +10,15 @@ using Microsoft.EntityFrameworkCore;
      public static IServiceCollection AddApplicationServices(this IServiceCollection services,
          IConfiguration config)
      {
-         services.AddControllers();
-         services.AddDbContext<DataContext>(opt =>
-         {
-             opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
-         });
-         services.AddCors();
-         services.AddScoped<ITokenService, TokenService>();
- 
-         return services;
+        services.AddControllers();
+        services.AddDbContext<DataContext>(opt =>
+        {
+            opt.UseSqlite(config.GetConnectionString("DefaultConnection"));
+        });
+        services.AddCors();
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<IUserRepository, UserRepository>();
+        services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+        return services;
      }
  }
