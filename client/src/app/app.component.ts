05253d0e6d4867fae106752ac6871c48f8ac0a0c
luigiantoine <lantoine@consultec-ti.com>
2025-04-16 23:06:36 -0400

    End of section 10

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavComponent } from "./nav/nav.component";
 import { AccountService } from './_services/account.service';
+import { NgxSpinnerComponent } from 'ngx-spinner';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-  imports: [RouterOutlet, NavComponent]
+  imports: [RouterOutlet, NavComponent, NgxSpinnerComponent]
 })
 export default class AppComponent implements OnInit {
   private accountService = inject(AccountService);
